61e5681d29abe67e7d9e9b722d2348c4e0fc6d6c
Maks Kowalski <kasrow12@gmail.com>
2024-03-03 15:16:17 +0100

    Lab02 - Finish

--- Lab02/Lab02.cs
+++ Lab02/Lab02.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ASD
 {
@@ -21,69 +22,72 @@ namespace ASD
         public (bool result, int cost, (int i, int j)[] path) Lab02Stage1(int n, int m,
             ((int di, int dj) step, int cost)[] moves)
         {
-            var T = new int[n, m];
-            var used = new int[n, m];
+            int[,] T = new int[n, m];
+            int[,] used = new int[n, m];
 
             // Warunki początkowe
-            for (var i = 0; i < n; i++)
-            for (var j = 0; j < m; j++)
-                T[i, j] = int.MaxValue;
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < m; j++)
+                    T[i, j] = int.MaxValue;
 
             T[0, 0] = 0;
 
             // Szukanie do przodu od góry
-            for (var i = 0; i < n; i++)
-            for (var j = 0; j < m; j++)
+            for (int i = 0; i < n; i++)
             {
-                if (T[i, j] == int.MaxValue)
-                    continue;
-
-                for (var k = 0; k < moves.Length; k++)
+                for (int j = 0; j < m; j++)
                 {
-                    var x = i + moves[k].step.di;
-                    var y = j + moves[k].step.dj;
-                    if (x >= n || y >= m)
+                    if (T[i, j] == int.MaxValue)
                         continue;
 
-                    if (T[i, j] + moves[k].cost < T[x, y])
+                    for (int k = 0; k < moves.Length; k++)
                     {
-                        T[x, y] = T[i, j] + moves[k].cost;
-                        used[x, y] = k;
+                        int x = i + moves[k].step.di;
+                        int y = j + moves[k].step.dj;
+                        if (x >= n || y >= m)
+                            continue;
+
+                        if (T[i, j] + moves[k].cost < T[x, y])
+                        {
+                            T[x, y] = T[i, j] + moves[k].cost;
+                            used[x, y] = k;
+                        }
                     }
                 }
             }
 
             // Wyszukanie kolumny z najmniejszym kosztem w ostatnim wierszu
-            var minCol = 0;
-            for (var j = 1; j < m; j++)
+            int minCol = 0;
+            for (int j = 1; j < m; j++)
+            {
                 if (T[n - 1, j] < T[n - 1, minCol])
                     minCol = j;
+            }
 
             // Nie ma trasy do ostatniego wiersza
             if (T[n - 1, minCol] == int.MaxValue)
                 return (false, int.MaxValue, null);
 
             // Wyszukiwanie ścieżki
-            var x1 = n - 1;
-            var y1 = minCol;
+            int x1 = n - 1;
+            int y1 = minCol;
             var path = new (int, int)[n + m];
 
-            var t = 0;
+            int t = 0;
             while (true)
             {
                 path[t++] = (x1, y1);
                 if (x1 == 0 && y1 == 0)
                     break;
 
-                var (di, dj) = moves[used[x1, y1]].step;
+                (int di, int dj) = moves[used[x1, y1]].step;
                 x1 -= di;
                 y1 -= dj;
             }
 
             // Odwrócenie ścieżki
             var newPath = new (int, int)[t];
-            for (var a = 0; a < t; a++)
-                newPath[a] = path[t - a - 1];
+            for (int a = 0; a < t; a++) newPath[a] = path[t - a - 1];
 
             return (true, T[n - 1, minCol], newPath);
         }
@@ -106,7 +110,74 @@ namespace ASD
         public (bool result, int cost, (int i, int j)[] pat) Lab02Stage2(int n, int m,
             ((int di, int dj) step, int cost)[] moves)
         {
-            return (false, int.MaxValue, null);
+            int[,,] T = new int[n, m, moves.Length + 1];
+
+            // Warunki początkowe
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < m; j++)
+                    for (int k = 0; k <= moves.Length; k++)
+                        T[i, j, k] = int.MaxValue;
+
+            for (int j = 0; j < m; j++)
+                T[n - 1, j, 0] = 0;
+
+            // Wyszukiwanie w tył
+            // Warto zauważyć, że kolejność ruchów nie ma znaczenia
+            for (int k = 1; k <= moves.Length; k++)
+            {
+                ((int di, int dj), int cost) = moves[k - 1];
+
+                for (int i = n - 1; i >= 0; i--)
+                {
+                    for (int j = m - 1; j >= 0; j--)
+                    {
+                        if (T[i, j, k - 1] == int.MaxValue)
+                            continue;
+
+                        // Przepisanie kosztu, taniej dostać się tutaj bez tego ruchu
+                        if (T[i, j, k] > T[i, j, k - 1])
+                            T[i, j, k] = T[i, j, k - 1];
+
+                        int x = i - di;
+                        int y = j - dj;
+                        if (x < 0 || y < 0)
+                            continue;
+
+                        // Czy użycie tego ruchu będzie tańsze niż trasa bez jego użycia
+                        if (T[x, y, k - 1] > T[i, j, k - 1] + cost)
+                            T[x, y, k] = T[i, j, k - 1] + cost;
+                    }
+                }
+            }
+
+            if (T[0, 0, moves.Length] == int.MaxValue)
+                return (false, int.MaxValue, null);
+
+            // Znajdowanie ścieżki
+            var path = new List<(int, int)> { (0, 0) };
+            int x1 = 0;
+            int y1 = 0;
+            int k1 = moves.Length;
+            while (true)
+            {
+                if (x1 == n - 1)
+                    break;
+
+                // Nie wykorzystano tego ruchu (koszt pozostał ten sam, a koszt ruchu nie może być zerem)
+                if (T[x1, y1, k1] == T[x1, y1, k1 - 1])
+                {
+                    k1--;
+                    continue;
+                }
+
+                (int di, int dj) = moves[k1 - 1].step;
+                x1 += di;
+                y1 += dj;
+                k1--;
+                path.Add((x1, y1));
+            }
+
+            return (true, T[0, 0, moves.Length], path.ToArray());
         }
     }
 }
