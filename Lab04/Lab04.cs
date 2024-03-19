66d8c1a73a1b1a473fbde433fd78e3d57b5de204
Maks Kowalski <kasrow12@gmail.com>
2024-03-19 19:27:21 +0100

    Lab04 - Finish

--- Lab04/Lab04.cs
+++ Lab04/Lab04.cs
@@ -1,97 +1,106 @@
 ﻿using ASD.Graphs;
+using System;
+using System.Collections.Generic;
 
-namespace ASD;
-
-public class Lab04 : MarshalByRefObject
+namespace ASD
 {
-    private int end;
-    private DiGraph g;
-    private int[] visited;
-
-    /// <summary>
-    ///     Etap 1 - Szukanie mozliwych do odwiedzenia miast z grafu skierowanego
-    ///     przy zalozeniu, ze pociagi odjezdzaja co godzine.
-    /// </summary>
-    /// <param name="graph">Graf skierowany przedstawiający siatke pociagow</param>
-    /// <param name="miastoStartowe">Numer miasta z ktorego zaczyna sie podroz pociagiem</param>
-    /// <param name="K">Godzina o ktorej musi zakonczyc sie nasza podroz</param>
-    /// <returns>Tablica numerow miast ktore mozna odwiedzic. Posortowana rosnaco.</returns>
-    public int[] Lab04Stage1(DiGraph graph, int miastoStartowe, int K)
+    public class Lab04 : MarshalByRefObject
     {
-        end = K;
-        visited = new int[graph.VertexCount];
-        for (int i = 0; i < graph.VertexCount; i++)
-            visited[i] = int.MaxValue;
-        g = graph;
-
-        Dfs(miastoStartowe, 8);
-
-        var list = new List<int>();
-        for (int i = 0; i < graph.VertexCount; i++)
+        /// <summary>
+        ///     Etap 1 - Szukanie mozliwych do odwiedzenia miast z grafu skierowanego
+        ///     przy zalozeniu, ze pociagi odjezdzaja co godzine.
+        /// </summary>
+        /// <param name="graph">Graf skierowany przedstawiający siatke pociagow</param>
+        /// <param name="miastoStartowe">Numer miasta z ktorego zaczyna sie podroz pociagiem</param>
+        /// <param name="K">Godzina o ktorej musi zakonczyc sie nasza podroz</param>
+        /// <returns>Tablica numerow miast ktore mozna odwiedzic. Posortowana rosnaco.</returns>
+        public int[] Lab04Stage1(DiGraph graph, int miastoStartowe, int K)
         {
-            if (visited[i] != int.MaxValue)
-                list.Add(i);
-        }
+            bool[] visited = new bool[graph.VertexCount];
 
-        return list.ToArray();
-    }
+            var q = new Queue<int>();
+            q.Enqueue(miastoStartowe);
+            visited[miastoStartowe] = true;
 
-    private void Dfs(int v, int h)
-    {
-        visited[v] = h;
-        foreach (int u in g.OutNeighbors(v))
-        {
-            if (h + 1 <= end && visited[u] > h + 1)
-                Dfs(u, h + 1);
-        }
-    }
-
-    /// <summary>
-    ///     Etap 2 - Szukanie mozliwych do odwiedzenia miast z grafu skierowanego.
-    ///     Waga krawedzi oznacza, ze pociag rusza o tej godzinie
-    /// </summary>
-    /// <param name="graph">Wazony graf skierowany przedstawiający siatke pociagow</param>
-    /// <param name="miastoStartowe">Numer miasta z ktorego zaczyna sie podroz pociagiem</param>
-    /// <param name="K">Godzina o ktorej musi zakonczyc sie nasza podroz</param>
-    /// <returns>Tablica numerow miast ktore mozna odwiedzic. Posortowana rosnaco.</returns>
-    public int[] Lab04Stage2(DiGraph<int> graph, int miastoStartowe, int K)
-    {
-        int[] visitedAt = new int[graph.VertexCount];
-        for (int i = 0; i < graph.VertexCount; i++)
-            visitedAt[i] = int.MaxValue;
-
-        var q = new PriorityQueue<int, (int, int)>();
-        q.Insert((miastoStartowe, 8), 8);
-
-        // Dijkstra
-        while (q.Count > 0)
-        {
-            (int v, int hour) = q.Extract();
-
-            if (visitedAt[v] <= hour)
-                continue;
-
-            visitedAt[v] = hour;
-            if (hour >= K)
-                continue;
-
-            foreach (var e in graph.OutEdges(v))
+            // Wykonujemy BFS'a tak głęboko, ile mamy godzin na jazdę
+            for (int i = 8; i < K; i++)
             {
-                // odjazd później && + godzina na dojazd && będziemy tam szybciej niż wcześniej
-                int arrival = e.Weight + 1;
-                if (hour <= e.Weight && arrival <= K && arrival < visitedAt[e.To])
-                    q.Insert((e.To, arrival), arrival);
+                // Należy zapamiętać ile było elementów w kolejce na tej głębokości, będą dochodzić w trakcie kolejne
+                for (int t = q.Count; t > 0; t--)
+                {
+                    int v = q.Dequeue();
+                    foreach (int u in graph.OutNeighbors(v))
+                    {
+                        if (!visited[u])
+                        {
+                            q.Enqueue(u);
+                            visited[u] = true;
+                        }
+                    }
+                }
             }
+
+            // Możliwe do odwiedzenia miasta. Przy okazji spełniamy warunek sortowania rosnącego
+            var cities = new List<int>();
+            for (int i = 0; i < graph.VertexCount; i++)
+            {
+                if (visited[i])
+                    cities.Add(i);
+            }
+
+            return cities.ToArray();
         }
 
-
-        var cities = new List<int>();
-        for (int i = 0; i < graph.VertexCount; i++)
+        /// <summary>
+        ///     Etap 2 - Szukanie mozliwych do odwiedzenia miast z grafu skierowanego.
+        ///     Waga krawedzi oznacza, ze pociag rusza o tej godzinie
+        /// </summary>
+        /// <param name="graph">Wazony graf skierowany przedstawiający siatke pociagow</param>
+        /// <param name="miastoStartowe">Numer miasta z ktorego zaczyna sie podroz pociagiem</param>
+        /// <param name="K">Godzina o ktorej musi zakonczyc sie nasza podroz</param>
+        /// <returns>Tablica numerow miast ktore mozna odwiedzic. Posortowana rosnaco.</returns>
+        public int[] Lab04Stage2(DiGraph<int> graph, int miastoStartowe, int K)
         {
-            if (visitedAt[i] != int.MaxValue)
-                cities.Add(i);
-        }
+            int[] visitedAt = new int[graph.VertexCount];
+            for (int i = 0; i < graph.VertexCount; i++)
+                visitedAt[i] = int.MaxValue;
 
-        return cities.ToArray();
+            var q = new PriorityQueue<int, (int, int)>();
+            q.Insert((miastoStartowe, 8), 8);
+            visitedAt[miastoStartowe] = 8;
+
+            // Dijkstra
+            while (q.Count > 0)
+            {
+                (int v, int hour) = q.Extract();
+
+                // Znaleziono już lepszą trasę, niż ta znajdująca się jeszcze w kolejce
+                if (hour > visitedAt[v])
+                    continue;
+
+                foreach (var e in graph.OutEdges(v))
+                {
+                    int arrival = e.Weight + 1;
+                    // Odjazd później && Dojazd w czasie && Będziemy tam szybciej niż dotychczas
+                    if (e.Weight >= hour && arrival <= K && arrival < visitedAt[e.To])
+                    {
+                        // Nie ma sensu dodawać, jak nigdzie stamtąd nie pojedziemy
+                        if (arrival != K)
+                            q.Insert((e.To, arrival), arrival);
+
+                        visitedAt[e.To] = arrival;
+                    }
+                }
+            }
+
+            var cities = new List<int>();
+            for (int i = 0; i < graph.VertexCount; i++)
+            {
+                if (visitedAt[i] != int.MaxValue)
+                    cities.Add(i);
+            }
+
+            return cities.ToArray();
+        }
     }
 }
