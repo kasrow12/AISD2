a9994f614b66142bdcb1e03af87294e860550594
Maks Kowalski <kasrow12@gmail.com>
2024-03-28 00:22:12 +0100

    Lab06-2022 Stage 1 & 2

---
+++ Lab06-2022/Lab06.cs
@@ -0,0 +1,249 @@
+﻿using ASD.Graphs;
+
+namespace ASD;
+
+public class Lab06 : MarshalByRefObject
+{
+    public int GetV(int i, int j, int l)
+    {
+        return i * l + j;
+    }
+
+    public (int, int) GetIJ(int v, int l)
+    {
+        return (v / l, v % l);
+    }
+
+    /// <summary>
+    ///     Etap 1 i 2 - szukanie trasy w nieplynacej rzece
+    /// </summary>
+    /// <param name="w"> Odległość między brzegami rzeki</param>
+    /// <param name="l"> Długość fragmentu rzeki </param>
+    /// <param name="lilie"> Opis lilii na rzece </param>
+    /// <param name="sila"> Siła żabki - maksymalny kwadrat długości jednego skoku </param>
+    /// <param name="start"> Początkowa pozycja w metrach od lewej strony </param>
+    /// <returns>
+    ///     (int total, (int x, int y)[] route) - total - suma sił koniecznych do wszystkich skoków, route -
+    ///     lista par opisujących skoki. Opis jednego skoku (x,y) to dystans w osi x i dystans w osi y, jaki skok pokonuje
+    /// </returns>
+    public (int total, (int, int)[] route) Lab06_FindRoute(int w, int l, int[,] lilie, int sila, int start)
+    {
+        var g = new DiGraph<int>(w * l + 2, new DictionaryGraphRepresentation()); // List
+        int s = w * l;
+        int e = w * l + 1;
+
+        // start
+        for (int i = 0; i < w; i++)
+        {
+            for (int j = 0; j < l; j++)
+            {
+                if (lilie[i, j] == 0)
+                    continue;
+
+                int dx = i + 1;
+                int dy = start - j;
+                int cost = dx * dx + dy * dy;
+                if (cost <= sila)
+                    g.AddEdge(s, GetV(i, j, l), cost);
+            }
+        }
+
+        // wszystko inne
+        for (int i = 0; i < w; i++)
+        {
+            for (int j = 0; j < l; j++)
+            {
+                if (lilie[i, j] == 0)
+                    continue;
+
+                int v = GetV(i, j, l);
+
+                for (int i2 = 0; i2 <= w; i2++)
+                {
+                    for (int j2 = 0; j2 < l; j2++)
+                    {
+                        // brzeg
+                        if (i2 == w)
+                        {
+                            int dx2 = i2 - i;
+                            int cost2 = dx2 * dx2;
+                            if (cost2 <= sila)
+                                g.AddEdge(v, e, cost2);
+                            continue;
+                        }
+
+                        if (lilie[i2, j2] == 0 || (i == i2 && j == j2))
+                            continue;
+
+                        int dx = i2 - i;
+                        int dy = j2 - j;
+                        int cost = dx * dx + dy * dy;
+                        if (cost <= sila)
+                            g.AddEdge(v, GetV(i2, j2, l), cost);
+                    }
+                }
+            }
+        }
+
+        var pathsInfo = Paths.Dijkstra(g, s);
+        int[]? path = pathsInfo.GetPath(s, e);
+        if (path == null)
+            return (0, null);
+
+        var route = new List<(int, int)>();
+        for (int t = 1; t < path.Length; t++)
+        {
+            (int ii, int jj) = GetIJ(path[t - 1], l);
+            (int i, int j) = GetIJ(path[t], l);
+            if (path[t - 1] == s) // start
+            {
+                ii = -1;
+                jj = start;
+            }
+
+            int dx = i - ii;
+            int dy = j - jj;
+            route.Add((dx, dy));
+        }
+
+        return (pathsInfo.GetDistance(s, e), route.ToArray());
+    }
+
+    /// <summary>
+    ///     Etap 3 i 4 - szukanie trasy w nieplynacej rzece
+    /// </summary>
+    /// <param name="w"> Odległość między brzegami rzeki</param>
+    /// <param name="l"> Długość fragmentu rzeki </param>
+    /// <param name="lilie"> Opis lilii na rzece </param>
+    /// <param name="sila"> Siła żabki - maksymalny kwadrat długości jednego skoku </param>
+    /// <param name="start"> Początkowa pozycja w metrach od lewej strony </param>
+    /// <param name="max_skok"> Maksymalna ilość skoków </param>
+    /// <param name="v"> Prędkość rzeki </param>
+    /// <returns>
+    ///     (int total, (int x, int y)[] route) - total - suma sił koniecznych do wszystkich skoków, route -
+    ///     lista par opisujących skoki. Opis jednego skoku (x,y) to dystans w osi x i dystans w osi y, jaki skok pokonuje
+    /// </returns>
+    public (int total, (int, int)[] route) Lab06_FindRouteFlowing(int w, int l, int[,] lilie, int sila, int start,
+        int max_skok, int v)
+    {
+        var g = new DiGraph<int>(w * l + 2, new DictionaryGraphRepresentation()); // List
+        int s = w * l;
+        int e = w * l + 1;
+
+        // start
+        for (int i = 0; i < w; i++)
+        {
+            for (int j = 0; j < l; j++)
+            {
+                if (lilie[i, j] == 0)
+                    continue;
+
+                int dx = i + 1;
+                int dy = start - j;
+                int cost = dx * dx + dy * dy;
+                if (cost <= sila)
+                    g.AddEdge(s, GetV(i, j, l), cost);
+            }
+        }
+
+        // wszystko inne
+        for (int i = 0; i < w; i++)
+        {
+            for (int j = 0; j < l; j++)
+            {
+                if (lilie[i, j] == 0)
+                    continue;
+
+                int vv = GetV(i, j, l);
+
+                for (int i2 = 0; i2 <= w; i2++)
+                {
+                    for (int j2 = 0; j2 < l; j2++)
+                    {
+                        // brzeg
+                        if (i2 == w)
+                        {
+                            int dx2 = i2 - i;
+                            int cost2 = dx2 * dx2;
+                            if (cost2 <= sila)
+                                g.AddEdge(vv, e, cost2);
+                            continue;
+                        }
+
+                        if (lilie[i2, j2] == 0 || (i == i2 && j == j2))
+                            continue;
+
+                        int dx = i2 - i;
+                        int dy = j2 - j;
+                        int cost = dx * dx + dy * dy;
+                        if (cost <= sila)
+                            g.AddEdge(vv, GetV(i2, j2, l), cost);
+                    }
+                }
+            }
+        }
+
+        // chyba warstwy jednak
+
+        int[] costs = new int[g.VertexCount];
+        for (int i = 0; i < g.VertexCount; i++)
+            costs[i] = int.MaxValue;
+
+        var q = new Queue<int>();
+        q.Enqueue(s);
+        costs[s] = 0;
+
+        int[] from = new int[g.VertexCount];
+
+        for (int i = 0; i < max_skok; i++)
+        {
+            for (int t = q.Count; t > 0; t--)
+            {
+                int vv = q.Dequeue();
+                foreach (int u in g.OutNeighbors(vv))
+                {
+                    int cst = costs[vv] + g.GetEdgeWeight(vv, u);
+                    if (cst < costs[u])
+                    {
+                        q.Enqueue(u);
+                        costs[u] = cst;
+                        from[u] = vv;
+                    }
+                }
+            }
+        }
+
+        if (costs[e] == int.MaxValue)
+            return (0, null);
+
+
+        var route = new List<(int, int)>();
+        int r = e;
+        int pr;
+        while (r != s)
+        {
+            pr = r;
+            r = from[r];
+            (int ii, int jj) = GetIJ(r, l);
+            (int i, int j) = GetIJ(pr, l);
+            if (r == s) // start
+            {
+                ii = -1;
+                jj = start;
+            }
+
+            int dx = i - ii;
+            int dy = j - jj;
+            route.Insert(0, (dx, dy));
+            Console.WriteLine($"{i} {j}: {(dx, dy)}");
+        }
+
+        // foreach (var x in route.ToArray())
+        // {
+        //     Console.WriteLine(x);
+        // }
+        // var yy = route.ToArray();
+
+        return (costs[e], route.ToArray());
+    }
+}
---
+++ Lab06-2022/Program.cs
@@ -0,0 +1,361 @@
+﻿using System;
+using ASD.Graphs;
+using ASD.Graphs.Testing;
+using System.Collections.Generic;
+
+namespace ASD
+{
+
+    public abstract class Lab06TestCase : TestCase
+    {
+        //protected readonly DiGraph<int> graph;
+        //protected readonly int daysNumber;
+        private readonly bool checkRoute;
+        protected readonly int expectedResult;
+
+        protected readonly int w;
+        protected readonly int l;
+        protected readonly int[,] lilies;
+        protected readonly int sila;
+        protected readonly int start;
+        protected readonly int max_skok;
+
+        protected (int total, (int,int)[] route) result;
+
+        //protected (bool res, int[] route) result;
+
+        public Lab06TestCase(int w, int l, int[,]lilies, int sila, int start, int max_skok, int expectedRes, double timeLimit, string description, bool checkR) : base(timeLimit, null, description)
+        { 
+            this.w = w;
+            this.l = l;
+            this.lilies = lilies;
+            this.sila = sila;
+            this.start = start;
+            this.max_skok = max_skok;
+            this.expectedResult = expectedRes;
+            this.checkRoute = checkR;
+        }
+
+        protected override (Result resultCode, string message) VerifyTestCase(object settings)
+        {
+            var (code, msg) = checkSolution(result.total, result.route);
+            return (code, $"{msg} [{this.Description}]");
+        }
+
+        private (Result resultCode, string message) checkSolution(int returnedResult, (int x, int y)[] returnedRoute)
+        {
+            if (expectedResult != returnedResult)
+            {
+                return (Result.WrongResult, $"Zwrócono {returnedResult}, powinno być {expectedResult}");
+            }
+
+            if (!checkRoute)
+            {
+                return OkResult("OK");
+            }
+
+            //zwracamy zero jesli nie ma trasy
+            if (returnedResult == 0)
+            {
+                if (returnedRoute != null)
+                    return (Result.WrongResult, "Nie ma trasy, należało zwrócić null");
+                return OkResult("OK");
+            }
+            if (returnedRoute == null)
+            {
+                return (Result.WrongResult, "Zwrócono null zamiast trasy");
+            }
+            if (returnedRoute.Length == 0)
+            {
+                return (Result.WrongResult, "Zwrócono pustą trasę");
+            }
+            return checkRouteValid(result.route);
+        }
+
+        protected abstract (Result resultCode, string message) checkRouteValid((int,int)[] returnedRoute);
+        public (Result resultCode, string message) OkResult(string message) => (TimeLimit < PerformanceTime ? Result.LowEfficiency : Result.Success, $"{message} {PerformanceTime.ToString("#0.00")}s");
+
+    }
+
+    public class Stage1TestCase : Lab06TestCase
+    {
+        public Stage1TestCase(int w, int l, int[,]lilies, int sila, int start, int max_skok, int expectedRes, double timeLimit, string description, bool checkR) : base(w, l, lilies, sila, start, max_skok, expectedRes, timeLimit, description, checkR)
+        {
+        }
+
+        protected override void PerformTestCase(object prototypeObject)
+        {
+            result = ((Lab06)prototypeObject).Lab06_FindRoute(w, l, lilies, sila, start);
+        }
+
+        protected override (Result resultCode, string message) checkRouteValid((int,int)[] returnedRoute)
+        {
+            int cur_x = -1;
+            int cur_y = start;
+            foreach ((int x, int y) jump in returnedRoute)
+            {
+                cur_x += jump.x;
+                cur_y += jump.y;
+                if(cur_x == w)
+                {
+                    return OkResult("OK");
+                }
+                if(lilies[cur_x,cur_y] != 1)
+                {
+                    return (Result.WrongResult, "Skok nie kończy się na lilii");
+                }
+            }
+            if(cur_x != w + 1)
+            {
+                return (Result.WrongResult, "Trasa nie kończy się na drugim brzegu");
+            }
+            return OkResult("OK");
+        }
+    }
+
+    public class Stage2TestCase : Lab06TestCase
+    {
+        private readonly int v;
+        public Stage2TestCase(int w, int l, int[,]lilies, int sila, int start, int max_skok, int v, int expectedRes, double timeLimit, string description, bool checkR) : base(w, l, lilies, sila, start, max_skok, expectedRes, timeLimit, description, checkR)
+        {
+            this.v = v;
+        }
+        protected override void PerformTestCase(object prototypeObject)
+        {
+            result = ((Lab06)prototypeObject).Lab06_FindRouteFlowing(w, l, lilies, sila, start, max_skok, v);
+        }
+        private int[,] Permute(int[,] rzeka, int t, int v)
+        {
+            int[,] ret = new int[w, l];
+            int permutation_dist = t * v % l;
+            for (int i = 0; i < w; i++)
+            {
+                for (int j = 0; j < l; j++)
+                {
+                    ret[i, (j + permutation_dist) % l ] = rzeka[i,j];
+                }
+            }
+            return ret;
+        }
+        protected override (Result resultCode, string message) checkRouteValid((int, int)[] returnedRoute)
+        {
+            if (returnedRoute.Length  > max_skok)
+            {
+                return (Result.WrongResult, "Zwrócono za długą trasę");
+            }
+            int cur_x = -1;
+            int cur_y = start;
+            int jump_count = 0;
+            foreach ((int x, int y) jump in returnedRoute)
+            {
+                cur_x += jump.x;
+                cur_y += jump.y;
+                jump_count++;
+                if (cur_x == -1) continue;
+                if(cur_x == w)
+                {
+                    return OkResult("OK");
+                }
+                if(Permute(lilies, jump_count, v)[cur_x,cur_y] != 1)
+                {
+                    return (Result.WrongResult, "Skok nie kończy się na lilii");
+                }
+            }
+            if(cur_x != w + 1)
+            {
+                return (Result.WrongResult, "Trasa nie kończy się na drugim brzegu");
+            }
+            return OkResult("OK");
+        }
+    }
+    public class Lab06Tests : TestModule
+    {
+        TestSet Stage1 = new TestSet(prototypeObject: new Lab06(), description: "Etap 1, minimalna siła żabki do pokonania trasy", settings: true);
+        TestSet Stage2 = new TestSet(prototypeObject: new Lab06(), description: "Etap 2, zwrócenie trasy z Etapu 1", settings: true);
+        TestSet Stage3 = new TestSet(prototypeObject: new Lab06(), description: "Etap 3, minimalna siła żabki do pokonania trasy na płynącej rzece", settings: true);
+        TestSet Stage4 = new TestSet(prototypeObject: new Lab06(), description: "Etap 4, zwrócenie trasy z Etapu 3", settings: true);
+
+        public override void PrepareTestSets()
+        {
+            TestSets["Stage1"] = Stage1;
+            TestSets["Stage2"] = Stage2;
+            TestSets["Stage3"] = Stage3;
+            TestSets["Stage4"] = Stage4;
+
+            prepare();
+        }
+
+        private void addStage1(Lab06TestCase s1aTestCase)
+        {
+            Stage1.TestCases.Add(s1aTestCase);
+        }
+
+        private void addStage2(Lab06TestCase s1bTestCase)
+        {
+            Stage2.TestCases.Add(s1bTestCase);
+        }
+
+        private void addStage3(Lab06TestCase s1aTestCase)
+        {
+            Stage3.TestCases.Add(s1aTestCase);
+        }
+
+        private void addStage4(Lab06TestCase s1bTestCase)
+        {
+            Stage4.TestCases.Add(s1bTestCase);
+        }
+
+        private void prepare()
+        {
+            int w;
+            int l;
+            int sila;
+            int start;
+            int max_skok;
+            int v;
+            int[,] lilie;
+
+            w = 3;
+            l = 3;
+            sila = 2;
+            lilie = new int[3,3] { { 1,0,1}, { 0,1,0}, { 1,0,1}};
+            max_skok = 6;
+            start = 1;
+            v = 0;
+
+            addStage1(new Stage1TestCase(w: w, l: l, lilies: lilie, sila: sila, start: start, max_skok: max_skok, expectedRes: 7, timeLimit: 10, description: "Przykład z zadania", checkR: false));
+            addStage2(new Stage1TestCase(w: w, l: l, lilies: lilie, sila: sila, start: start, max_skok: max_skok, expectedRes: 7, timeLimit: 10, description: "Przykład z zadania", checkR: true));
+            addStage3(new Stage2TestCase(w: w, l: l, lilies: lilie, sila: sila, start: start, max_skok: max_skok, v: v, expectedRes: 7, timeLimit: 10, description: "Przykład z zadania", checkR: false));
+            addStage4(new Stage2TestCase(w: w, l: l, lilies: lilie, sila: sila, start: start, max_skok: max_skok, v: v, expectedRes: 7, timeLimit: 10, description: "Przykład z zadania", checkR: true));
+
+
+            w = 2;
+            l = 2;
+            sila = 10;
+            lilie = new int[2, 2] { { 1, 0 }, { 0, 1 } };
+            max_skok = 4;
+            start = 0;
+            v = 0;
+            addStage1(new Stage1TestCase(w: w, l: l, lilies: lilie, sila: sila, start: start, max_skok: max_skok, expectedRes: 4, timeLimit: 10, description: "Przykład prosty", checkR: false));
+            addStage2(new Stage1TestCase(w: w, l: l, lilies: lilie, sila: sila, start: start, max_skok: max_skok, expectedRes: 4, timeLimit: 10, description: "Przykład prosty", checkR: true));
+            addStage3(new Stage2TestCase(w: w, l: l, lilies: lilie, sila: sila, start: start, max_skok: max_skok, v: v, expectedRes: 4, timeLimit: 10, description: "Przykład prosty", checkR: false));
+            addStage4(new Stage2TestCase(w: w, l: l, lilies: lilie, sila: sila, start: start, max_skok: max_skok, v: v, expectedRes: 4, timeLimit: 10, description: "Przykład prosty", checkR: true));
+
+            w = 4;
+            l = 8;
+            sila = 10;
+            lilie = new int[4, 8] { { 1, 0, 1, 0, 1, 0, 1, 0 },
+                                    { 1, 0, 0, 1, 0, 0, 1, 0 }, 
+                                    { 0, 0, 1, 1, 1, 1, 1, 1 },
+                                    { 1, 0, 1, 1, 0, 0, 1, 1 }};
+            max_skok = 6;
+            start = 2;
+            v = 0;
+            addStage1(new Stage1TestCase(w: w, l: l, lilies: lilie, sila: sila, start: start, max_skok: max_skok, expectedRes: 6, timeLimit: 10, description: "Przykład większy", checkR: false));
+            addStage2(new Stage1TestCase(w: w, l: l, lilies: lilie, sila: sila, start: start, max_skok: max_skok, expectedRes: 6, timeLimit: 10, description: "Przykład większy", checkR: true));
+            addStage3(new Stage2TestCase(w: w, l: l, lilies: lilie, sila: sila, start: start, max_skok: max_skok, v: v, expectedRes: 6, timeLimit: 10, description: "Przykład większy", checkR: false));
+            addStage4(new Stage2TestCase(w: w, l: l, lilies: lilie, sila: sila, start: start, max_skok: max_skok, v: v, expectedRes: 6, timeLimit: 10, description: "Przykład większy", checkR: true));
+
+            w = 10;
+            l = 2;
+            sila = 5;
+            lilie = new int[10, 2] { { 1, 0 }, { 1, 0 }, { 1, 0 }, { 1, 0 }, { 1, 0 }, { 1, 0 }, { 1, 0 }, { 1, 0 }, { 1, 0 }, { 1, 0 } };
+            max_skok = 9;
+            start = 0;
+            v = 0;
+
+            addStage1(new Stage1TestCase(w: w, l: l, lilies: lilie, sila: sila, start: start, max_skok: max_skok, expectedRes: 11, timeLimit: 10, description: "Przykład wąska rzeka", checkR: false));
+            addStage2(new Stage1TestCase(w: w, l: l, lilies: lilie, sila: sila, start: start, max_skok: max_skok, expectedRes: 11, timeLimit: 10, description: "Przykład wąska rzeka", checkR: true));
+            addStage3(new Stage2TestCase(w: w, l: l, lilies: lilie, sila: sila, start: start, max_skok: max_skok, v: v, expectedRes: 15, timeLimit: 10, description: "Przykład długie skoki", checkR: false));
+            addStage4(new Stage2TestCase(w: w, l: l, lilies: lilie, sila: sila, start: start, max_skok: max_skok, v: v, expectedRes: 15, timeLimit: 10, description: "Przykład długie skoki", checkR: true));
+
+            w = 8;
+            l = 11;
+            sila = 4;
+            max_skok = 14;
+            lilie = new int[8, 11] { { 1,0,0,0,0,0,0,0,0,0,0},
+                                      {1,0,0,0,1,0,1,0,1,0,1},
+                                      {1,0,0,0,1,0,0,0,0,0,0},
+                                      {1,0,1,0,1,0,0,0,0,0,1},
+                                      {0,0,0,0,0,0,0,0,0,0,0},
+                                      {0,0,0,0,0,0,0,0,0,0,1},
+                                      {0,0,0,0,0,0,0,0,0,0,0},
+                                      {0,0,0,0,0,0,0,0,0,0,1}
+            };
+            start = 0;
+            v = 0;
+
+            addStage1(new Stage1TestCase(w: w, l: l, lilies: lilie, sila: sila, start: start, max_skok: max_skok, expectedRes: 39, timeLimit: 40, description: "Przykład skomplikowany", checkR: false));
+            addStage2(new Stage1TestCase(w: w, l: l, lilies: lilie, sila: sila, start: start, max_skok: max_skok, expectedRes: 39, timeLimit: 40, description: "Przykład skomplikowany", checkR: true));
+            addStage3(new Stage2TestCase(w: w, l: l, lilies: lilie, sila: sila, start: start, max_skok: max_skok, v: v, expectedRes: 41, timeLimit: 50, description: "Przykład skomplikowany", checkR: false));
+            addStage4(new Stage2TestCase(w: w, l: l, lilies: lilie, sila: sila, start: start, max_skok: max_skok, v: v, expectedRes: 41, timeLimit: 50, description: "Przykład skomplikowany", checkR: true));
+
+            w = 4;
+            l = 7;
+            sila = 2;
+            lilie = new int[4, 7] { { 1,0,1,0,1,0,1},
+                                    { 1,0,0,1,0,0,1},
+                                    { 1,0,0,0,1,0,0},
+                                    { 1,0,0,0,0,1,0} } ;
+            max_skok = 8;
+            start = 0;
+            v = 0;
+            addStage1(new Stage1TestCase(w: w, l: l, lilies: lilie, sila: sila, start: start, max_skok: max_skok, expectedRes: 5, timeLimit: 10, description: "Przykład szybka rzeka", checkR: false));
+            addStage2(new Stage1TestCase(w: w, l: l, lilies: lilie, sila: sila, start: start, max_skok: max_skok, expectedRes: 5, timeLimit: 10, description: "Przykład szybka rzeka", checkR: true));
+            addStage3(new Stage2TestCase(w: w, l: l, lilies: lilie, sila: sila, start: start, max_skok: max_skok, v: v, expectedRes: 5, timeLimit: 10, description: "Przykład szybka rzeka", checkR: false));
+            addStage4(new Stage2TestCase(w: w, l: l, lilies: lilie, sila: sila, start: start, max_skok: max_skok, v: v, expectedRes: 5, timeLimit: 10, description: "Przykład szybka rzeka", checkR: true));
+
+            Random rng = new Random(777);
+            w = 20;
+            l = 20;
+            sila = 7;
+            lilie = new int[20, 20];
+            for(int i=0; i<w; i++)
+            {
+                for(int j=0; j<l; j++)
+                {
+                    lilie[i,j] = rng.NextDouble() > 0.9 ? 1 : 0;
+                }
+            }
+            max_skok = 80;
+            start = 10;
+            v = 0;
+            addStage1(new Stage1TestCase(w: w, l: l, lilies: lilie, sila: sila, start: start, max_skok: max_skok, expectedRes: 0, timeLimit: 10, description: "Przykład rzadka losowa rzeka", checkR: false));
+            addStage2(new Stage1TestCase(w: w, l: l, lilies: lilie, sila: sila, start: start, max_skok: max_skok, expectedRes: 0, timeLimit: 10, description: "Przykład rzadka losowa rzeka", checkR: true));
+            addStage3(new Stage2TestCase(w: w, l: l, lilies: lilie, sila: sila, start: start, max_skok: max_skok, v: v, expectedRes: 0, timeLimit: 10, description: "Przykład rzadka losowa rzeka", checkR: false));
+            addStage4(new Stage2TestCase(w: w, l: l, lilies: lilie, sila: sila, start: start, max_skok: max_skok, v: v, expectedRes: 0, timeLimit: 10, description: "Przykład rzadka losowa rzeka", checkR: true));
+
+            w = 10;
+            l = 10;
+            sila = 3;
+            lilie = new int[10, 10];
+            for(int i=0; i<w; i++)
+            {
+                for(int j=0; j<l; j++)
+                {
+                    lilie[i,j] = rng.NextDouble() > 0.6 ? 1 : 0;
+                }
+            }
+            max_skok = 13;
+            start = 5;
+            v = 0;
+            addStage1(new Stage1TestCase(w: w, l: l, lilies: lilie, sila: sila, start: start, max_skok: max_skok, expectedRes: 17, timeLimit: 10, description: "Przykład gęsta losowa rzeka", checkR: false));
+            addStage2(new Stage1TestCase(w: w, l: l, lilies: lilie, sila: sila, start: start, max_skok: max_skok, expectedRes: 17, timeLimit: 10, description: "Przykład gęsta losowa rzeka", checkR: true));
+            addStage3(new Stage2TestCase(w: w, l: l, lilies: lilie, sila: sila, start: start, max_skok: max_skok, v: v, expectedRes: 17, timeLimit: 10, description: "Przykład gęsta losowa rzeka", checkR: false));
+            addStage4(new Stage2TestCase(w: w, l: l, lilies: lilie, sila: sila, start: start, max_skok: max_skok, v: v, expectedRes: 17, timeLimit: 10, description: "Przykład gęsta losowa rzeka", checkR: true));
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tests = new Lab06Tests();
+            tests.PrepareTestSets();
+            foreach (var ts in tests.TestSets)
+            {
+                ts.Value.PerformTests(verbose: true, checkTimeLimit: false);
+            }
+        }
+    }
+}
