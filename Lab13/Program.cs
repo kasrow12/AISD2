b08411708938ec3db5af35ff8321dc42d6fb19af
Maks Kowalski <kasrow12@gmail.com>
2024-05-23 20:03:21 +0200

    Lab13 - idk

---
+++ Lab13/Program.cs
@@ -0,0 +1,183 @@
+﻿using ASD;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab15
+{
+    class Program
+    {
+        class PeriodTestCase : TestCase
+        {
+            string s;
+            int expectedResult;
+            int result;
+
+            public PeriodTestCase(string s, int result, double timeLimit)
+                : base(timeLimit, null, null)
+            {
+                this.s = s;
+                expectedResult = result;
+            }
+
+
+            protected override void PerformTestCase(object prototypeObject)
+            {
+                result = s.Period();
+            }
+
+            protected override (Result resultCode, string message) VerifyTestCase(object settings)
+            {
+                Result resultCode;
+                string message;
+                if (expectedResult != result)
+                {
+                    resultCode = Result.WrongResult;
+                    message = "Error: answer = " + result + "\texpected = " + expectedResult;
+                }
+                else
+                {
+                    resultCode = Result.Success;
+                    message = "OK";
+                }
+                return (resultCode, message);
+            }
+        }
+
+        class PowerTestCase : TestCase
+        {
+            string s;
+            int expectedResult;
+            int result, startI, endI;
+
+            public PowerTestCase(string s, int result, double timeLimit)
+                : base(timeLimit, null, null)
+            {
+                this.s = s;
+                expectedResult = result;
+            }
+
+            protected override void PerformTestCase(object prototypeObject)
+            {
+                result = s.MaxPower(out startI, out endI);
+            }
+
+            protected override (Result resultCode, string message) VerifyTestCase(object settings)
+            {
+                Result resultCode;
+                string message;
+                if (expectedResult != result)
+                {
+                    resultCode = Result.WrongResult;
+                    message = "Error: answer = " + result + "\texpected = " + expectedResult;
+                }
+                else
+                {
+                    bool ok = true;
+
+                    if (startI < 0 || endI < startI || endI > s.Length)
+                        ok = false;
+                    else if ((endI - startI) % result != 0)
+                        ok = false;
+                    else
+                    {
+                        int len = (endI - startI) % result;
+                        for (int i = startI; i < endI - len; i++)
+                            if (s[i] != s[i + len])
+                                ok = false;
+                    }
+                    if (ok)
+                    {
+                        resultCode = Result.Success;
+                        message = "OK";
+                    }
+                    else
+                    {
+                        resultCode = Result.WrongResult;
+                        message = "Answer ok, wrong indices [" + startI + "," + endI + ")";
+                    }
+                }
+
+                return (resultCode, message);
+            }
+        }
+
+        static string ntimes(string s, int n)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < n; i++)
+                sb.Append(s);
+            return sb.ToString();
+        }
+
+        static string makeRandom(int len, int seed, int alphabet)
+        {
+            StringBuilder sb = new StringBuilder();
+            Random rand = new Random(seed);
+            for (int i = 0; i < len; i++)
+                sb.Append((char)('a' + rand.Next(alphabet)));
+            return sb.ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            // Console.WriteLine(stringExtender.KMP("abc", "abcababab"));
+            // var x = stringExtender.ComputePrefix("ababacabab");
+            
+            
+            // Console.WriteLine("Okres slowa - male testy");
+            TestSet smallPeriod = new TestSet(null, "Okres slowa - male testy");
+            smallPeriod.TestCases.Add(new PeriodTestCase("a", 1, 1));
+            smallPeriod.TestCases.Add(new PeriodTestCase("aa", 1, 1));
+            smallPeriod.TestCases.Add(new PeriodTestCase("ab", 2, 1));
+            smallPeriod.TestCases.Add(new PeriodTestCase("abcdabc", 4, 1));
+            smallPeriod.TestCases.Add(new PeriodTestCase("abcdef", 6, 1));
+            smallPeriod.TestCases.Add(new PeriodTestCase("abacabaxabacaba", 8, 1));
+            smallPeriod.TestCases.Add(new PeriodTestCase("abcdabcdabcde", 13, 1));
+            smallPeriod.TestCases.Add(new PeriodTestCase("eabcdabcdabcd", 13, 1));
+            smallPeriod.TestCases.Add(new PeriodTestCase("aaaabaaabaa", 9, 1));
+            smallPeriod.PerformTests(true, false);
+
+            // Console.WriteLine("Okres slowa - duze testy");
+            TestSet bigPeriod = new TestSet(null, "Okres slowa - duze testy");
+            int n1 = 50000;
+            string rand1, rand2;
+            rand1 = makeRandom(n1, 13, 32);
+            rand2 = makeRandom(n1, 3218, 2);
+            bigPeriod.TestCases.Add(new PeriodTestCase(rand1 + rand1.Substring(0, n1 / 2), n1, 1));
+            bigPeriod.TestCases.Add(new PeriodTestCase(rand1.Substring(n1 / 2) + rand1, n1, 1));
+            bigPeriod.TestCases.Add(new PeriodTestCase(rand2, 49985, 1));
+            bigPeriod.TestCases.Add(new PeriodTestCase(ntimes("abc", n1 / 3), 3, 1));
+            bigPeriod.TestCases.Add(new PeriodTestCase(ntimes(makeRandom(200, 12, 3), n1 / 200), 200, 1));
+            bigPeriod.PerformTests(true, true);
+
+            //Console.WriteLine("Najwieksza potega - male testy");
+            TestSet smallPower = new TestSet(null, "Najwieksza potega - male testy");
+            smallPower.TestCases.Add(new PowerTestCase("aaaxyzaxyzxyzaxyzxyzaxyzxyzaxyzxyzaaa", 4, 1));
+            smallPower.TestCases.Add(new PowerTestCase("a", 1, 1));
+            smallPower.TestCases.Add(new PowerTestCase("ab", 1, 1));
+            smallPower.TestCases.Add(new PowerTestCase("aa", 2, 1));
+            smallPower.TestCases.Add(new PowerTestCase("xyzabxyza", 1, 1));
+            smallPower.TestCases.Add(new PowerTestCase("xyzabxyzabxyzabxyza", 3, 1));
+            smallPower.TestCases.Add(new PowerTestCase("abacabadabacaba", 1, 1));
+            smallPower.TestCases.Add(new PowerTestCase("aaaxyzaxyzxyzaxyzxyzaxyzxyzaxyzxyzaaa", 4, 1));
+            smallPower.TestCases.Add(new PowerTestCase("abcabcabcaaxxyy", 3, 1));
+            smallPower.TestCases.Add(new PowerTestCase("xyyxaabcabcabca", 3, 1));
+            smallPower.PerformTests(true, false);
+
+            //Console.WriteLine("Najwieksza potega - duze testy");
+            TestSet bigPower = new TestSet(null, "Najwieksza potega - duze testy");
+            int n2 = 5000;
+            string rand3 = makeRandom(n2, 13, 32);
+            string rand4 = makeRandom(n2, 14, 2);
+            bigPower.TestCases.Add(new PowerTestCase(rand3, 3, 2));
+            bigPower.TestCases.Add(new PowerTestCase(rand4, 11, 3));
+            bigPower.TestCases.Add(new PowerTestCase(ntimes("abc", n2 / 3), n2 / 3, 2));
+            bigPower.TestCases.Add(new PowerTestCase(makeRandom(n2 / 4, 11, 3) + ntimes("abc", n2 / 6) + makeRandom(n2 / 4, 11, 4), n2 / 6, 3));
+            bigPower.TestCases.Add(new PowerTestCase(makeRandom(n2 / 4, 11, 3) + ntimes(makeRandom(70, 17, 3), n2 / 70) + makeRandom(n2 / 4, 11, 4), n2 / 70, 4));
+            bigPower.PerformTests(false, true);
+
+        }
+    }
+}
---
+++ Lab13/StringExtender.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Text;
+
+namespace Lab15
+{
+    public static class stringExtender
+    {
+        /// <summary>
+        /// Metoda zwraca okres słowa s, tzn. najmniejszą dodatnią liczbę p taką, że s[i]=s[i+p] dla każdego i od 0 do |s|-p-1.
+        /// 
+        /// Metoda musi działać w czasie O(|s|)
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        static public int Period(this string s)
+        {
+            return s.Length - ComputePrefix(s)[s.Length - 1];
+        }
+
+        /// <summary>
+        /// Metoda wyznacza największą potęgę zawartą w słowie s.
+        /// 
+        /// Jeżeli x jest słowem, wówczas przez k-tą potęgę słowa x rozumiemy k-krotne powtórzenie słowa x
+        /// (na przykład xyzxyzxyz to trzecia potęga słowa xyz).
+        /// 
+        /// Należy zwrócić największe k takie, że k-ta potęga jakiegoś słowa jest zawarta w s jako spójny podciąg.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="startIndex">Pierwszy indeks fragmentu zawierającego znalezioną potęgę</param>
+        /// <param name="endIndex">Pierwszy indeks po fragmencie zawierającym znalezioną potęgę</param>
+        /// <returns></returns>
+        static int[] makeP(string s)
+        {
+            int[] P = new int[s.Length + 1];
+            int t = 0;
+
+            for(int i=2;i<=s.Length;i++)
+            {
+                while (s[t] != s[i-1] && t > 0)
+                    t = P[t];
+                P[i] = t;
+                if (s[i - 1] == s[t])
+                    P[i] = ++t;
+            }
+            return P;
+        }
+        static public int MaxPower(this string s, out int startIndex, out int endIndex)
+        {
+            if (s == null || s.Length == 0)
+            {
+                startIndex = endIndex = 0;
+                return 0;
+            }
+            
+            startIndex = 0;
+            endIndex = 1;
+            int maxPow = 1;
+            int n = s.Length;
+            
+            for (int i = 0; i < n; i++)
+            {
+                int[] prefix = ComputePrefix(s[i..]);
+                for (int j = 2; j <= n - i; j++)
+                {
+                    if (j % (j - prefix[j - 1]) == 0 && j / (j - prefix[j - 1]) > maxPow)
+                    {
+                        maxPow = j / (j - prefix[j - 1]);
+                        startIndex = i;
+                        endIndex = i + j;
+                    }
+                }
+            }
+
+            return maxPow;
+        }
+
+
+        public static int[] ComputePrefix(string pattern)
+        {
+            int length = 0;
+            int i = 1;
+            int[] lps = new int[pattern.Length];
+            lps[0] = 0;
+
+            while (i < pattern.Length)
+            {
+                if (pattern[i] == pattern[length])
+                {
+                    length++;
+                    lps[i] = length;
+                    i++;
+                }
+                else
+                {
+                    if (length != 0)
+                    {
+                        length = lps[length - 1];
+                    }
+                    else
+                    {
+                        lps[i] = 0;
+                        i++;
+                    }
+                }
+            }
+            return lps;
+        }
+        
+        // static public int[] ComputePrefix(string pattern)
+        // {
+        //     int[] preifx = new int[pattern.Length + 1];
+        //     int k = 0;
+        //     for (int q = 2; q < pattern.Length; q++)
+        //     {
+        //         while (k > 0 && pattern[k + 1] != pattern[q])
+        //             k = preifx[k];
+        //
+        //         if (pattern[k + 1] == pattern[q])
+        //             k++;
+        //
+        //         preifx[q] = k;
+        //     }
+        //
+        //     return preifx[1..];
+        // }
+        
+        static public bool KMP(string pattern, string text)
+        {
+            int[] prefix = ComputePrefix(pattern);
+            int q = 0;
+            for (int i = 1; i < pattern.Length; i++)
+            {
+                while (q > 0 && pattern[q + 1] != text[i])
+                    q = prefix[q];
+                
+                if (pattern[q + 1] == text[i])
+                    q++;
+                
+                if (q == pattern.Length)
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
