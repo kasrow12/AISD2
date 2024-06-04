7d0d8723a8bd6d3bbd72d7c530a5ff80cb5efcb7
Maks Kowalski <kasrow12@gmail.com>
2024-06-04 14:16:09 +0200

    Lab15 - temp

--- Lab15/GraphColorer.cs
+++ Lab15/GraphColorer.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using ASD.Graphs;
 
 namespace ASD2
@@ -13,7 +14,58 @@ namespace ASD2
         /// <returns>Liczba użytych kolorów i kolorowanie (coloring[i] to kolor wierzchołka i). Kolory mogą być dowolnymi liczbami całkowitymi.</returns>
         public (int numberOfColors, int[] coloring) FindBestColoring(Graph g)
         {
-            return (0, null);
+            int n = g.VertexCount;
+            if (n == 0)
+                return (0, null);
+            
+            int[] coloring = new int[n];
+            int colors = 1;
+
+            bool IsSafe(int v, int c)
+            {
+                foreach (int i in g.OutNeighbors(v))
+                    if (coloring[i] == c)
+                        return false;
+                return true;
+            }
+            
+            int[] vertices = Enumerable.Range(0, n).ToArray();
+            Array.Sort(vertices, (v1, v2) => g.OutNeighbors(v1).Count().CompareTo(g.OutNeighbors(v2).Count()));
+            
+            bool ColorGraph(int i)
+            {
+                // Base case: If all vertices are assigned a color then return true
+                if (i == n)
+                    return true;
+
+                // Consider this vertex v and try different colors
+                for (int c = 1; c <= colors; c++)
+                {
+                    // Check if assignment of color c to v is fine
+                    if (IsSafe(vertices[i], c))
+                    {
+                        coloring[vertices[i]] = c;
+
+                        // Recur to assign colors to the rest of the vertices
+                        if (ColorGraph(i + 1))
+                            return true;
+
+                        // If assigning color c doesn't lead to a solution then remove it (backtrack)
+                        coloring[vertices[i]] = 0;
+                    }
+                }
+
+                // If no color can be assigned to this vertex then return false
+                return false;
+            }
+
+            while (!ColorGraph(0) && colors <= n)
+            {
+                Console.WriteLine(colors);
+                colors++;
+            }
+            
+            return (colors, coloring);
         }
 
     }
